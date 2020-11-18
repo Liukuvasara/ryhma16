1f60691a046b7553178e5ffc62156aed02b82534
Lauri Vuorenkoski <lauri.vuorenkoski@helsinki.fi>
2020-11-18 16:45:06 +0200

    uuden kirjan lisääminen

--- Lukuvinkisto/src/main/java/Lukuvinkisto/io/BookIO.java
+++ Lukuvinkisto/src/main/java/Lukuvinkisto/io/BookIO.java
@@ -5,6 +5,7 @@
  */
 package lukuvinkisto.io;
 
+import Lukuvinkisto.Book;
 import java.util.List;
 
 /**
@@ -15,4 +16,17 @@ public class BookIO {
     public static List fetchBooks(){
         return null;
     }
+    
+    public static Book NewBookTip() {
+        System.out.println("Lisätään uusi kirja");
+        System.out.println("Anna kirjan otsikko: ");
+        String title = InputIO.readInput();
+        System.out.println("Anna kirjan tekijä (Sukunimi, Etunimi): ");
+        String author = InputIO.readInput();
+        System.out.println("Anna kirjan sivumäärä: ");
+        int pages = Integer.parseInt(InputIO.readInput());
+        Book book = new Book(title, author, pages);
+        return book;
+    }
+
 }
